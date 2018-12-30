0ba5e67bc21cd26bb0cf2f9c158c6015d426251c
samokasha <okasha86@hotmail.com>
2018-12-30 19:56:28 +0400

    Add Mui, add styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
-import countryCodes from "./data/countries";
-import axios from "axios";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 
-const App = () => {
+import axios from "axios";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  CardHeader,
+  Typography
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import countryCodes from "./data/countries";
+
+const App = ({ classes }) => {
   const [weather, setWeather] = useState(null);
   const [countryCode, setCountryCode] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
@@ -29,8 +45,9 @@ const App = () => {
     }
   };
 
-  const fetchWeather = async city => {
+  const fetchWeather = async (e, city) => {
     try {
+      e.preventDefault();
       if (searchQuery) {
         if (countryCode) city += `,${countryCode}`;
         const { data } = await axios.get(
@@ -103,41 +120,75 @@ const App = () => {
   return (
     <div>
       <br />
-      <label htmlFor="city">City:</label>
-      <input
-        name="city"
-        onChange={e => {
-          setSearchQuery(e.target.value);
-        }}
-        value={searchQuery}
-      />
-      <label htmlFor="countries_select">Country (Optional):</label>
-      <select onChange={e => onChangeCountry(e)} id="countries_select">
-        <option value="" />
-        {countryCodes.map(country => (
-          <option key={country.Code} value={country.Code}>
-            {country.Name}
-          </option>
-        ))}
-      </select>
-      <button
-        variant="contained"
-        onClick={() => fetchWeather(searchQuery)}
-        disabled={searchQuery.length < 1}
+
+      <form
+        className={classes.form}
+        onSubmit={e => fetchWeather(e, searchQuery)}
       >
-        Search
-      </button>
+        <FormControl>
+          {/* <label htmlFor="city">City:</label> */}
+          <TextField
+            label="City"
+            name="city"
+            onChange={e => {
+              setSearchQuery(e.target.value);
+            }}
+            value={searchQuery}
+          />
+        </FormControl>
+
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="countries_select">
+            Country (optional):
+          </InputLabel>
+          <Select
+            native
+            value={countryCode}
+            inputProps={{
+              name: "countries_select",
+              id: "countries_select"
+            }}
+            onChange={e => onChangeCountry(e)}
+          >
+            <option value="" />
+            {countryCodes.map(country => (
+              <option key={country.Code} value={country.Code}>
+                {country.Name}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+
+        <Button
+          type="submit"
+          variant="contained"
+          disabled={searchQuery.length < 1}
+        >
+          Search
+        </Button>
+      </form>
+
       {weather && (
-        <div>
-          <h3>
+        <Card className={classes.searchCard}>
+          <CardHeader>
             {weather.name}, {weather.sys.country}
-          </h3>
-          <p>{weather.main.temp}</p>
-          <p>{weather.weather[0].description}</p>
-          {!cityIds.includes(weather.id) && (
-            <button onClick={() => onSaveCity(weather.id)}>Save</button>
-          )}
-        </div>
+          </CardHeader>
+          <CardContent>
+            <p>{weather.main.temp}</p>
+            <p>{weather.weather[0].description}</p>
+          </CardContent>
+          <CardActions>
+            {!cityIds.includes(weather.id) && (
+              <Button
+                size="small"
+                color="primary"
+                onClick={() => onSaveCity(weather.id)}
+              >
+                Save
+              </Button>
+            )}
+          </CardActions>
+        </Card>
       )}
       {message && (
         <div>
@@ -147,17 +198,43 @@ const App = () => {
       {fetchingCities && <div>Fetching your cities...</div>}
       {citiesData &&
         citiesData.map(weather => (
-          <div key={weather.id}>
-            <h3>
-              {weather.name}, {weather.sys.country}
-            </h3>
-            <p>{weather.main.temp}</p>
-            <p>{weather.weather[0].description}</p>
-            <button onClick={() => removeCity(weather.id)}>Remove</button>
-          </div>
+          <Card className={classes.searchCard}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {weather.name}, {weather.sys.country}
+              </Typography>
+              <Typography component="p">
+                Current temperature: {weather.main.temp} &#8451;
+              </Typography>
+              <Typography component="p">
+                {weather.weather[0].description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={() => removeCity(weather.id)}>
+                Remove
+              </Button>
+              size="small"
+            </CardActions>
+          </Card>
         ))}
     </div>
   );
 };
 
-export default App;
+const styles = theme => ({
+  form: {
+    width: "50%",
+    display: "flex",
+    alignItems: "center"
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  },
+  searchCard: {
+    width: "250px"
+  }
+});
+
+export default withStyles(styles)(App);
